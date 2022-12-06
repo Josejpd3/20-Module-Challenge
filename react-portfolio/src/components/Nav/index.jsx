f17135ceced716faf0f9711f24c8d1b08d88aef1
Jose Plasencia <josejpd3@gmail.com>
2022-12-06 17:46:23 -0500

    about onClick in src/components/Nav/index.jsx

--- react-portfolio/src/components/Nav/index.jsx
+++ react-portfolio/src/components/Nav/index.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Nav = ({ showAbout, showProjects, showContact }) => {
     return (
         <div>
-            <h1>About</h1>
+            <h1 onClick={showAbout}>About</h1>
         </div>
     );
 }
