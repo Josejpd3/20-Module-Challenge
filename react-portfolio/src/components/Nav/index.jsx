e025e6ce85960f1db801b2366cd07358f617c53b
Jose Plasencia <josejpd3@gmail.com>
2022-12-06 23:35:12 -0500

    styled nav with bootstrap in src/components/index.jsx

--- react-portfolio/src/components/Nav/index.jsx
+++ react-portfolio/src/components/Nav/index.jsx
@@ -1,12 +1,23 @@
 import React from "react";
+import Container from 'react-bootstrap/Container';
+import NavSec from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Nav = ({ showAbout, showProjects, showContact }) => {
     return (
-        <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-            <h1 onClick={showAbout}>About</h1>
-            <h1 onClick={showProjects}>Projects</h1>
-            <h1 onClick={showContact}>Contact</h1>
-        </div>
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Container>
+          <Navbar.Toggle aria-controls="responsive-navbar-NavSec" />
+          <Navbar.Collapse id="responsive-navbar-NavSec" >
+            <NavSec className="me-auto justify-content-md-center">
+              <NavSec.Link onClick={showAbout}>About</NavSec.Link>
+              <NavSec.Link onClick={showProjects}>Projects</NavSec.Link>
+              <NavSec.Link onClick={showContact}>Contact</NavSec.Link>
+            </NavSec>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     );
 }
 
