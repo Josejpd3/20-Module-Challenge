02481b80516fc9c95555b873490ef3066c4efe16
Jose Plasencia <josejpd3@gmail.com>
2022-12-07 23:22:31 -0500

    about section component content in src/components/About/index.jsx

--- react-portfolio/src/components/About/index.jsx
+++ react-portfolio/src/components/About/index.jsx
@@ -3,7 +3,11 @@ import './about.css'
 
 const About = () => {
     return (
-        <div>About</div>
+        <div className="about">
+            <h2>About Me</h2>
+            <img src="../../logo.svg" alt="Jose Plasencia Profile" />
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
     );
 }
 
