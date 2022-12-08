da6ef14dad3ea024a1aafd793ea45db9c47cb733
Jose Plasencia <josejpd3@gmail.com>
2022-12-08 15:37:04 -0500

    main contact form structure in src/components/Contact/index.jsx

--- react-portfolio/src/components/Contact/index.jsx
+++ react-portfolio/src/components/Contact/index.jsx
@@ -2,7 +2,24 @@ import React from "react";
 
 const Contact = () => {
     return (
-        <div>Contact</div>
+            <div>
+                <form id="contactForm">
+                    <div>
+                        <label htmlFor="name">Name</label>
+                        <input id="name" type="text" placeholder="Name" required/>
+                    </div>
+                    <div>
+                        <label htmlFor="emailAddress">Email Address</label>
+                        <input id="emailAddress" type="email" placeholder="Email Address" required/>
+                    </div>
+                    <div>
+                        <label htmlFor="message">Message</label>
+                        <textarea id="message" type="text" placeholder="Message" required></textarea>
+                    </div>
+
+                    <button className="submitBtn" type="submit">Submit</button>
+                </form>
+            </div>
     );
 }
 
