1dd9f3ff07eb86d9bbec3fecd9c0df5473966e94
Jose Plasencia <josejpd3@gmail.com>
2022-12-07 23:18:51 -0500

    removed bootstrap import and bootstrap content setup in src/components/Nav/index.jsx

--- react-portfolio/src/components/Nav/index.jsx
+++ react-portfolio/src/components/Nav/index.jsx
@@ -1,25 +1,18 @@
 import React from "react";
-import Container from 'react-bootstrap/Container';
-import NavSec from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './nav.css';
 
 const Nav = ({ showAbout, showProjects, showContact, showResume }) => {
     return (
-        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Container>
-          <Navbar.Toggle aria-controls="responsive-navbar-NavSec" />
-          <Navbar.Collapse id="responsive-navbar-NavSec" >
-            <NavSec className="me-auto justify-content-md-center">
-              <NavSec.Link onClick={showAbout}>About</NavSec.Link>
-              <NavSec.Link onClick={showProjects}>Projects</NavSec.Link>
-              <NavSec.Link onClick={showContact}>Contact</NavSec.Link>
-              <NavSec.Link onClick={showResume}>Resume</NavSec.Link>
-            </NavSec>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+      <header>
+        <h1>Jose Plasencia</h1>
+        <nav>
+          <button onClick={showAbout}>About Me</button>
+          <button onClick={showProjects}>Projects</button>
+          <button onClick={showContact}>Contact Me</button>
+          <button onClick={showResume}>Resume</button>
+        </nav>
+      </header>
     );
 }
 
