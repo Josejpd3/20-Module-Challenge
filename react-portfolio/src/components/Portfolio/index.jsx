1b78ff949d89283cfd57e211c888f61dd6eaec4f
Jose Plasencia <josejpd3@gmail.com>
2022-12-08 21:29:31 -0500

    project img layout and link in src/components/Portfolio/index.jsx

--- react-portfolio/src/components/Portfolio/index.jsx
+++ react-portfolio/src/components/Portfolio/index.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import './portfolio.css';
 
 
 const Portfolio = ({projects}) => {
@@ -9,6 +10,9 @@ const Portfolio = ({projects}) => {
         {projects.map((project) => (
             <div key={project.name} className="card m-3" style={{width: "900px"}}>
                 <div className="row g-0">
+                <div className="col-md-4 project-image-container">
+                    <a href={project.deployed}><img src={project.imgPath} className="project-img img-fluid rounded-start" alt={project.name}/></a>
+                </div>
                 <div className="col-md-8">
                     <div className="card-body">
 
