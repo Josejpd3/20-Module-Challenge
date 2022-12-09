620afa097908c7eca73a7e43c55b0537a0576d02
Jose Plasencia <josejpd3@gmail.com>
2022-12-08 21:28:36 -0500

    main portfolio structure in src/components/Portfolio/index.jsx

--- react-portfolio/src/components/Portfolio/index.jsx
+++ react-portfolio/src/components/Portfolio/index.jsx
@@ -1,8 +1,23 @@
 import React from "react";
 
-const Portfolio = () => {
+
+const Portfolio = ({projects}) => {
+
+
     return (
-        <div>Portfolio</div>
+    <div style={{display: 'flex', flexWrap: 'wrap'}}>
+        {projects.map((project) => (
+            <div key={project.name} className="card m-3" style={{width: "900px"}}>
+                <div className="row g-0">
+                <div className="col-md-8">
+                    <div className="card-body">
+
+                    </div>
+                </div>
+                </div>
+            </div>
+        ))}
+    </div>
     );
 }
 
