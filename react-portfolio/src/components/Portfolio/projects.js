127c0b6b2ed2ba27880fc0b128b4a6613183fe15
Jose Plasencia <josejpd3@gmail.com>
2022-12-08 21:24:17 -0500

    import icons in src/components/Portfolio/projects.js

--- react-portfolio/src/components/Portfolio/projects.js
+++ react-portfolio/src/components/Portfolio/projects.js
@@ -2,4 +2,19 @@
 import cinemafiles from './screenshots/cinemafiles.jpg';
 import nomadicAdventures from './screenshots/nomadic-adventures.jpg';
 
+// icons
 
+import api from './icons/api-svgrepo-com.svg';
+import bulma from './icons/bulma.svg';
+import css from './icons/css.svg';
+import html from './icons/html.svg';
+import expressjs from './icons/expressjs-icon.svg';
+// import bootstrap from './icons/getbootstrap-icon.svg';
+import git from './icons/git-scm-icon.svg';
+// import bash from './icons/gnu_bash-icon.svg';
+import handlebarsjs from './icons/handlebarsjs-icon.svg';
+import javascript from './icons/javascript.svg';
+import jquery from './icons/jquery-icon.svg';
+import mysql from './icons/mysql-icon.svg';
+import nodejs from './icons/nodejs-icon.svg';
+import sequelizejs from './icons/sequelize-seeklogo.com.svg';
