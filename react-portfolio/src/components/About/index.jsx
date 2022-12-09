7f05c50e3353838311393920a47a303e6e78cd55
Jose Plasencia <josejpd3@gmail.com>
2022-12-09 17:31:47 -0500

    profile img styling in src/components/About/index.jsx

--- react-portfolio/src/components/About/index.jsx
+++ react-portfolio/src/components/About/index.jsx
@@ -6,7 +6,7 @@ const About = () => {
     return (
         <div className="about">
             <h2>About Me</h2>
-            <img src="../../logo.svg" alt="Jose Plasencia Profile" />
+            <img src={img} style={{width: '104px', border: '2px solid black', borderRadius: '50%'}} alt="Jose Plasencia Profile" />
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </div>
     );
