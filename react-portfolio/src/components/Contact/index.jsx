3d7eeb7ce51d8db3989ad28c229ad36188f2bdc6
Jose Plasencia <josejpd3@gmail.com>
2022-12-08 23:38:57 -0500

    contact background animation in src/components/Contact/index.jsx

--- react-portfolio/src/components/Contact/index.jsx
+++ react-portfolio/src/components/Contact/index.jsx
@@ -3,24 +3,21 @@ import './contact.css';
 
 const Contact = () => {
     return (
-            <div>
-                <form id="contactForm">
-                    <div class="mb-3">
-                        <label htmlFor="name">Name</label>
-                        <input class="form-control" id="name" type="text" placeholder="Name" required/>
-                    </div>
-                    <div class="mb-3">
-                        <label class="form-label" htmlFor="emailAddress">Email Address</label>
-                        <input class="form-control" id="emailAddress" type="email" placeholder="Email Address" required/>
-                    </div>
-                    <div class="mb-3">
-                        <label class="form-label" htmlFor="message">Message</label>
-                        <textarea class="form-control" id="message" type="text" placeholder="Message" required></textarea>
-                    </div>
-
-                    <button className="submitBtn btn btn-primary" type="submit">Submit</button>
-                </form>
-            </div>
+        <div>
+            
+            <ul class="background">
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
     );
 }
 
