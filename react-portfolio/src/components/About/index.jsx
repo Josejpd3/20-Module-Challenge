e6211703eb9b2f1e97df3258f89011fcd8acc4a7
Jose Plasencia <josejpd3@gmail.com>
2022-12-09 17:32:28 -0500

    about me paragraph in src/components/About/index.jsx

--- react-portfolio/src/components/About/index.jsx
+++ react-portfolio/src/components/About/index.jsx
@@ -7,7 +7,7 @@ const About = () => {
         <div className="about">
             <h2>About Me</h2>
             <img src={img} style={{width: '104px', border: '2px solid black', borderRadius: '50%'}} alt="Jose Plasencia Profile" />
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <p>located in Kissimmee, FL, with over 4 years of experience developing, designing, testing, and deploying web and desktop applications. I have constructed numerous applications for local businesses, wanting to either showcase their services on the web or manage their business efficiently by utilizing personalized software that I have developed. At the same time, having constant communication with these clients to ensure that their requirements and specifications are met with competence and precision. Writing structured code along with insightful comments and documentation in order for other developers to easily jump in and maintain these applications.</p>
         </div>
     );
 }
