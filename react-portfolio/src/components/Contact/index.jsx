15308cdb91d3b717aea30990834c8500f13a7932
Jose Plasencia <josejpd3@gmail.com>
2022-12-08 23:40:36 -0500

    bootstrapped form in src/components/Contact/index.jsx

--- react-portfolio/src/components/Contact/index.jsx
+++ react-portfolio/src/components/Contact/index.jsx
@@ -16,6 +16,23 @@ const Contact = () => {
                 <li></li>
                 <li></li>
                 <li></li>
+                <div style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
+                    <form id="contactForm">
+                        <h3>Contact Me</h3>
+                        <div class="mb-3">
+                            <label htmlFor="name">Name</label>
+                            <input class="form-control" id="name" type="text" placeholder="Name" required/>
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label" htmlFor="emailAddress">Email Address</label>
+                            <input class="form-control" id="emailAddress" type="email" placeholder="Email Address" required/>
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label" htmlFor="message">Message</label>
+                            <textarea class="form-control" id="message" type="text" placeholder="Message" required></textarea>
+                        </div>
+                    </form>
+                </div>
             </ul>
         </div>
     );
